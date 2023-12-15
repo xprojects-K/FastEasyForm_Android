7b641d1a46da4a4dd1322e51b17a3e0913f10634
José I. Gutiérrez B <jose19.26@hotmail.com>
2023-12-15 13:57:28 -0500

    Add change

---
+++ formsimple/build/tmp/kapt3/stubs/release/com/creativity/dev/formsimple/model/Utils/EditText.java
@@ -0,0 +1,19 @@
+package com.creativity.dev.formsimple.model.Utils;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 8, 0}, k = 1, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u000b\n\u0002\b\u0004\b\u0007\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002R\u001a\u0010\u0003\u001a\u00020\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0003\u0010\u0005\"\u0004\b\u0006\u0010\u0007\u00a8\u0006\b"}, d2 = {"Lcom/creativity/dev/formsimple/model/Utils/EditText;", "", "()V", "isEditable", "", "()Z", "setEditable", "(Z)V", "formsimple_release"})
+public final class EditText {
+    private boolean isEditable = true;
+    
+    public EditText() {
+        super();
+    }
+    
+    public final boolean isEditable() {
+        return false;
+    }
+    
+    public final void setEditable(boolean p0) {
+    }
+}
---
+++ formsimple/build/tmp/kapt3/stubs/release/com/creativity/dev/formsimple/model/Utils/Visibility.java
@@ -0,0 +1,91 @@
+package com.creativity.dev.formsimple.model.Utils;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 8, 0}, k = 1, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b \b\u0007\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002R\u001a\u0010\u0003\u001a\u00020\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0005\u0010\u0006\"\u0004\b\u0007\u0010\bR\u001a\u0010\t\u001a\u00020\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\n\u0010\u0006\"\u0004\b\u000b\u0010\bR\u001a\u0010\f\u001a\u00020\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\r\u0010\u0006\"\u0004\b\u000e\u0010\bR\u001a\u0010\u000f\u001a\u00020\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0010\u0010\u0006\"\u0004\b\u0011\u0010\bR\u001a\u0010\u0012\u001a\u00020\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0013\u0010\u0006\"\u0004\b\u0014\u0010\bR\u001a\u0010\u0015\u001a\u00020\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0016\u0010\u0006\"\u0004\b\u0017\u0010\bR\u001a\u0010\u0018\u001a\u00020\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0019\u0010\u0006\"\u0004\b\u001a\u0010\bR\u001a\u0010\u001b\u001a\u00020\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u001c\u0010\u0006\"\u0004\b\u001d\u0010\bR\u001a\u0010\u001e\u001a\u00020\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u001f\u0010\u0006\"\u0004\b \u0010\bR\u001a\u0010!\u001a\u00020\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\"\u0010\u0006\"\u0004\b#\u0010\b\u00a8\u0006$"}, d2 = {"Lcom/creativity/dev/formsimple/model/Utils/Visibility;", "", "()V", "check", "", "getCheck", "()I", "setCheck", "(I)V", "contentCheck", "getContentCheck", "setContentCheck", "description", "getDescription", "setDescription", "editText", "getEditText", "setEditText", "icon", "getIcon", "setIcon", "imgContentEnable", "getImgContentEnable", "setImgContentEnable", "imgContentEnable2", "getImgContentEnable2", "setImgContentEnable2", "separator", "getSeparator", "setSeparator", "styleEdit", "getStyleEdit", "setStyleEdit", "title", "getTitle", "setTitle", "formsimple_release"})
+public final class Visibility {
+    private int description = android.view.View.GONE;
+    private int title = android.view.View.VISIBLE;
+    private int icon = android.view.View.GONE;
+    private int check = android.view.View.GONE;
+    private int editText = android.view.View.GONE;
+    private int separator = android.view.View.INVISIBLE;
+    private int styleEdit = android.view.View.GONE;
+    private int contentCheck = android.view.View.GONE;
+    private int imgContentEnable = android.view.View.GONE;
+    private int imgContentEnable2 = android.view.View.VISIBLE;
+    
+    public Visibility() {
+        super();
+    }
+    
+    public final int getDescription() {
+        return 0;
+    }
+    
+    public final void setDescription(int p0) {
+    }
+    
+    public final int getTitle() {
+        return 0;
+    }
+    
+    public final void setTitle(int p0) {
+    }
+    
+    public final int getIcon() {
+        return 0;
+    }
+    
+    public final void setIcon(int p0) {
+    }
+    
+    public final int getCheck() {
+        return 0;
+    }
+    
+    public final void setCheck(int p0) {
+    }
+    
+    public final int getEditText() {
+        return 0;
+    }
+    
+    public final void setEditText(int p0) {
+    }
+    
+    public final int getSeparator() {
+        return 0;
+    }
+    
+    public final void setSeparator(int p0) {
+    }
+    
+    public final int getStyleEdit() {
+        return 0;
+    }
+    
+    public final void setStyleEdit(int p0) {
+    }
+    
+    public final int getContentCheck() {
+        return 0;
+    }
+    
+    public final void setContentCheck(int p0) {
+    }
+    
+    public final int getImgContentEnable() {
+        return 0;
+    }
+    
+    public final void setImgContentEnable(int p0) {
+    }
+    
+    public final int getImgContentEnable2() {
+        return 0;
+    }
+    
+    public final void setImgContentEnable2(int p0) {
+    }
+}
