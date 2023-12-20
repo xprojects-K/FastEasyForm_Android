1cba64319c50212940be86a0f04e0b9a351bf0c4
José I. Gutiérrez B <jose19.26@hotmail.com>
2023-12-19 22:20:25 -0500

    add
    add

---
+++ formsimple/build/tmp/kapt3/stubs/debug/com/creativity/dev/formsimple/BuilderFormsKt.java
@@ -0,0 +1,17 @@
+package com.creativity.dev.formsimple;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 8, 0}, k = 2, d1 = {"\u0000\"\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0018\u0002\n\u0002\u0010\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u001a-\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u00032\u0017\u0010\u0004\u001a\u0013\u0012\u0004\u0012\u00020\u0001\u0012\u0004\u0012\u00020\u00060\u0005\u00a2\u0006\u0002\b\u0007H\u0086\b\u00f8\u0001\u0000\u001a%\u0010\b\u001a\u00020\u00062\u0017\u0010\u0004\u001a\u0013\u0012\u0004\u0012\u00020\t\u0012\u0004\u0012\u00020\u00060\u0005\u00a2\u0006\u0002\b\u0007H\u0086\b\u00f8\u0001\u0000\u0082\u0002\u0007\n\u0005\b\u009920\u0001\u00a8\u0006\n"}, d2 = {"Row", "Lcom/creativity/dev/formsimple/model/ListDynamic;", "type", "", "obj", "Lkotlin/Function1;", "", "Lkotlin/ExtensionFunctionType;", "option", "Lcom/creativity/dev/formsimple/model/Select;", "formsimple_debug"})
+public final class BuilderFormsKt {
+    
+    @org.jetbrains.annotations.NotNull()
+    public static final com.creativity.dev.formsimple.model.ListDynamic Row(int type, @org.jetbrains.annotations.NotNull()
+    kotlin.jvm.functions.Function1<? super com.creativity.dev.formsimple.model.ListDynamic, kotlin.Unit> obj) {
+        return null;
+    }
+    
+    public static final void option(@org.jetbrains.annotations.NotNull()
+    kotlin.jvm.functions.Function1<? super com.creativity.dev.formsimple.model.Select, kotlin.Unit> obj) {
+    }
+}
