ea560ea70a335c431229d2acbdf4fa2aa553f187
José I. Gutiérrez B <jose19.26@hotmail.com>
2023-12-20 14:36:21 -0500

    add
    chane

---
+++ formsimple/build/tmp/kapt3/stubs/release/com/creativity/dev/formsimple/model/Utils/Text.java
@@ -0,0 +1,77 @@
+package com.creativity.dev.formsimple.model.Utils;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 8, 0}, k = 1, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0002\b\u0014\b\u0007\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002R\u001a\u0010\u0003\u001a\u00020\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0005\u0010\u0006\"\u0004\b\u0007\u0010\bR\u001a\u0010\t\u001a\u00020\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\n\u0010\u0006\"\u0004\b\u000b\u0010\bR\u001a\u0010\f\u001a\u00020\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\r\u0010\u0006\"\u0004\b\u000e\u0010\bR\u001a\u0010\u000f\u001a\u00020\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0010\u0010\u0006\"\u0004\b\u0011\u0010\bR\u001a\u0010\u0012\u001a\u00020\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0013\u0010\u0006\"\u0004\b\u0014\u0010\bR\u001a\u0010\u0015\u001a\u00020\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0016\u0010\u0006\"\u0004\b\u0017\u0010\b\u00a8\u0006\u0018"}, d2 = {"Lcom/creativity/dev/formsimple/model/Utils/Text;", "", "()V", "comment", "", "getComment", "()Ljava/lang/String;", "setComment", "(Ljava/lang/String;)V", "edtHint", "getEdtHint", "setEdtHint", "emptyMessages", "getEmptyMessages", "setEmptyMessages", "icon", "getIcon", "setIcon", "text", "getText", "setText", "title", "getTitle", "setTitle", "formsimple_release"})
+public final class Text {
+    @org.jetbrains.annotations.NotNull()
+    private java.lang.String title = "";
+    @org.jetbrains.annotations.NotNull()
+    private java.lang.String text = "";
+    @org.jetbrains.annotations.NotNull()
+    private java.lang.String edtHint = "";
+    @org.jetbrains.annotations.NotNull()
+    private java.lang.String comment = "";
+    @org.jetbrains.annotations.NotNull()
+    private java.lang.String icon = "";
+    @org.jetbrains.annotations.NotNull()
+    private java.lang.String emptyMessages = "Please Insert Text";
+    
+    public Text() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getTitle() {
+        return null;
+    }
+    
+    public final void setTitle(@org.jetbrains.annotations.NotNull()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getText() {
+        return null;
+    }
+    
+    public final void setText(@org.jetbrains.annotations.NotNull()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getEdtHint() {
+        return null;
+    }
+    
+    public final void setEdtHint(@org.jetbrains.annotations.NotNull()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getComment() {
+        return null;
+    }
+    
+    public final void setComment(@org.jetbrains.annotations.NotNull()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getIcon() {
+        return null;
+    }
+    
+    public final void setIcon(@org.jetbrains.annotations.NotNull()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getEmptyMessages() {
+        return null;
+    }
+    
+    public final void setEmptyMessages(@org.jetbrains.annotations.NotNull()
+    java.lang.String p0) {
+    }
+}
